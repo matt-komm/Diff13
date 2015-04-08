62b6dd2eb3059c6897068a8330dfbf4cb9889fce
Georgios Krintiras <gkrintir@nikhef.nl>
2015-04-08 20:11:21 +0200

    Partial Update after Matthias having merged

---
+++ PxlModules/selection/TriggerSelection.cpp
@@ -0,0 +1,138 @@
+#include "pxl/hep.hh"
+#include "pxl/core.hh"
+#include "pxl/core/macros.hh"
+#include "pxl/core/PluginManager.hh"
+#include "pxl/modules/Module.hh"
+#include "pxl/modules/ModuleFactory.hh"
+
+static pxl::Logger logger("TriggerSelection");
+
+class TriggerSelection:
+    public pxl::Module
+{
+    private:
+        pxl::Source* _output1TriggerSource;
+        pxl::Source* _outputOtherTriggerSource;
+
+        std::string _inputEventViewName;
+        std::string _triggerName;
+
+        bool _cleanEvent;
+        
+    public:
+        TriggerSelection():
+            Module(),
+	    _inputEventViewName("Reconstructed"),
+            _triggerName("HLT_IsoMu24_eta2p1_IterTrk02_v1"),
+            _cleanEvent(true)
+
+        {
+            addSink("input", "input");
+            _output1TriggerSource = addSource("Isolated Single Lepton", "Isolated Single Lepton");
+            _outputOtherTriggerSource = addSource("other", "other");
+
+            addOption("Event view","name of the event view",_inputEventViewName);
+            addOption("Trigger of selected events","",_triggerName);
+
+            addOption("Clean event","this option will clean the events falling out of trigger",_cleanEvent);
+        }
+
+        ~TriggerSelection()
+        {
+        }
+
+        // every Module needs a unique type
+        static const std::string &getStaticType()
+        {
+            static std::string type ("TriggerSelection");
+            return type;
+        }
+
+        // static and dynamic methods are needed
+        const std::string &getType() const
+        {
+            return getStaticType();
+        }
+
+        bool isRunnable() const
+        {
+            // this module does not provide events, so return false
+            return false;
+        }
+
+        void initialize() throw (std::runtime_error)
+        {
+        }
+
+        void beginJob() throw (std::runtime_error)
+        {
+            getOption("Event view",_inputEventViewName);
+            addOption("Trigger of selected events","",_triggerName);
+            
+	    getOption("Clean event",_cleanEvent);
+            
+        }
+
+        bool passesSingleIsoLeptonTriggerSelection( pxl::EventView* eventView)
+        {
+	    if (eventView->hasUserRecord(_triggerName.data())) {
+	        if (not eventView->getUserRecord(_triggerName.data())) {
+	            return false;
+		}
+		
+		return true; 
+	    }
+	}
+
+        bool analyse(pxl::Sink *sink) throw (std::runtime_error)
+        {
+            try
+            {
+                pxl::Event *event  = dynamic_cast<pxl::Event*>(sink->get());
+                if (event)
+                {
+                    std::vector<pxl::EventView*> eventViews;
+                    event->getObjectsOfType(eventViews);
+                    
+                    for (unsigned ieventView=0; ieventView<eventViews.size();++ieventView)
+                    {
+
+                        pxl::EventView* eventView = eventViews[ieventView];
+                        if (eventView->getName()==_inputEventViewName)
+                        {
+			    if (passesSingleIsoLeptonTriggerSelection(eventView)) {
+			        _output1TriggerSource->setTargets(event);
+                                return _output1TriggerSource->processTargets();
+			    }else {
+			        _outputOtherTriggerSource->setTargets(event);
+                                return _outputOtherTriggerSource->processTargets();
+			    }
+			}
+		    }
+		}
+	    }
+            catch(std::exception &e)
+            {
+                throw std::runtime_error(getName()+": "+e.what());
+            }
+            catch(...)
+            {
+                throw std::runtime_error(getName()+": unknown exception");
+            }
+
+            logger(pxl::LOG_LEVEL_ERROR , "Analysed event is not an pxl::Event !");
+            return false;
+        }
+
+        void shutdown() throw(std::runtime_error)
+        {
+        }
+
+        void destroy() throw (std::runtime_error)
+        {
+            delete this;
+        }
+};
+
+PXL_MODULE_INIT(TriggerSelection)
+PXL_PLUGIN_INIT
