6d7e8b81bcb7aa4180f729b3f09e1d29ff1acd68
Matthias <mkomm@cern.ch>
2015-04-15 14:00:15 +0200

    crab config

---
+++ CMSSW/crab/main.py
@@ -0,0 +1,72 @@
+from CRABAPI.RawCommand import crabCommand
+from httplib import HTTPException
+from CRABClient.UserUtilities import config
+
+
+def submit(config,*args,**kwargs):
+    try:
+        crabCommand('submit', args, kwargs, config = config)
+    except HTTPException, hte:
+        print hte.headers
+
+if __name__=="__main__":
+    config = config()
+
+    config.General.requestName = 'ST_EA13'
+    #config.General.workArea = 'crab/TToLeptons_t-channel-CSA14_Tune4C_13TeV-aMCatNLO-tauola'
+    config.General.transferOutputs = True
+    config.General.transferLogs = False
+
+    config.JobType.pluginName = 'Analysis'
+    config.JobType.psetName = 'EDM2PXLIO/phys14.py'
+    config.JobType.pyCfgParams = []
+    config.JobType.outputFiles = ["output.pxlio"]
+
+    #config.Data.inputDataset = '/TToLeptons_t-channel-CSA14_Tune4C_13TeV-aMCatNLO-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
+    config.Data.inputDBS = 'global'
+    config.Data.splitting = 'FileBased'
+    config.Data.unitsPerJob = 5
+    #config.Data.outLFN = '/store/user/mkomm'
+    config.Data.publication = False
+
+
+    config.Site.storageSite = "T2_BE_UCL"
+    config.Site.whitelist = ['T2_CH_CERN','T2_DE_DESY','T2_BE_IIHE','T2_BE_UCL','T2_IT_Legnaro','T2_IT_Rome']
+    
+    for dataset in [
+        "/TToLeptons_t-channel-CSA14_Tune4C_13TeV-aMCatNLO-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM",
+        #"/TBarToLeptons_t-channel_Tune4C_CSA14_13TeV-aMCatNLO-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM",
+        
+        #"/T_tW-channel-DR_Tune4C_13TeV-CSA14-powheg-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM",
+        #"/Tbar_tW-channel-DR_Tune4C_13TeV-CSA14-powheg-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM",
+        
+        #"/TToLeptons_s-channel-CSA14_Tune4C_13TeV-aMCatNLO-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM",
+        #"/TBarToLeptons_s-channel-CSA14_Tune4C_13TeV-aMCatNLO-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM",
+        
+        #"/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM",
+        
+        #"/WJetsToLNu_13TeV-madgraph-pythia8-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM",
+        
+        #"/WJetsToLNu_HT-100to200_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM",
+        #"/WJetsToLNu_HT-200to400_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM",
+        #"/WJetsToLNu_HT-400to600_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM",
+        #"/WJetsToLNu_HT-700toInf_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM",
+        
+        #"/DYJetsToLL_M-50_13TeV-madgraph-pythia8/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM",
+        
+        #"/QCD_Pt-20toInf_MuEnrichedPt15_PionKaonDecay_Tune4C_13TeV_pythia8/Phys14DR-PU20bx25_PHYS14_25_V1-v3/MINIAODSIM",
+        #"/QCD_Pt_20to30_bcToE_Tune4C_13TeV_pythia8/Phys14DR-PU20bx25_PHYS14_25_V1-v2/MINIAODSIM",
+        #"/QCD_Pt_30to80_bcToE_Tune4C_13TeV_pythia8/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM",
+        #"/QCD_Pt_80to170_bcToE_Tune4C_13TeV_pythia8/Phys14DR-PU20bx25_PHYS14_25_V1-v2/MINIAODSIM",
+        #"/QCD_Pt_170toInf_bcToE_Tune4C_13TeV_pythia8/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM",
+        #"/QCD_Pt-20to30_EMEnriched_Tune4C_13TeV_pythia8/Phys14DR-PU20bx25_castor_PHYS14_25_V1-v2/MINIAODSIM",
+        #"/QCD_Pt-30to80_EMEnriched_Tune4C_13TeV_pythia8/Phys14DR-PU20bx25_castor_PHYS14_25_V1-v1/MINIAODSIM",
+        #"/QCD_Pt-80to170_EMEnriched_Tune4C_13TeV_pythia8/Phys14DR-PU20bx25_castor_PHYS14_25_V1-v1/MINIAODSIM",
+    ]:
+        jobName = dataset.split("/")[1]+'_v2'
+        print "submitting... ",jobName
+        config.General.workArea = 'crab/'+jobName
+        config.Data.inputDataset=dataset
+        config.Data.outLFN='/store/user/mkomm/'+config.General.requestName+"/"+jobName
+        submit(config)
+
