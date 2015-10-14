734e2ebf890bfc22af6bb4644bcc710447464752
Matthias <mkomm@cern.ch>
2015-10-14 13:51:57 +0200

    update CMSSW version

--- CMSSW/setupCMSSW.sh
+++ CMSSW/setupCMSSW.sh
@@ -30,7 +30,7 @@ cd $BASEDIR/CMSSW
 export SCRAM_ARCH=slc6_amd64_gcc491
 addVar SCRAM_ARCH $SCRAM_ARCH
 
-CMSSWVERSION="CMSSW_7_4_12_patch4"
+CMSSWVERSION="CMSSW_7_4_15"
 
 execute "scramv1 project CMSSW $CMSSWVERSION"
 cd $CMSSWVERSION/src
@@ -40,7 +40,7 @@ execute "git cms-init"
 
 execute "git cms-merge-topic ikrav:egm_id_7.4.12_v1"
 
-execute "git clone -b CMSSW_7412_patch4 https://github.com/matt-komm/EDM2PXLIO.git"
+execute "git clone -b CMSSW_7415 https://github.com/matt-komm/EDM2PXLIO.git"
 execute "git reset EDM2PXLIO"
 execute "git clone https://github.com/matt-komm/Pxl.git"
 execute "git reset Pxl"
