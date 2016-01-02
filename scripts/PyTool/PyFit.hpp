9e5f3a3e7406a0c64acdd1c2abe43128ff99d6be
matt-komm <Matthias.Komm@cern.ch>
2016-01-02 23:01:17 +0100

    adding minimizer

--- scripts/PyTool/PyFit.hpp
+++ scripts/PyTool/PyFit.hpp
@@ -7,6 +7,9 @@
 #include "TF1.h"
 #include "TMath.h"
 
+#include "Minuit2/Minuit2Minimizer.h"
+#include "Math/Functor.h"
+
 #include <iostream>
 #include <vector>
 #include <map>
@@ -32,6 +35,7 @@ class PyFit
             public:
                 Parameter(const char* name):
                     _name(name),
+                    _scaleFactor(1.0),
                     _min(std::numeric_limits<double>::lowest()),
                     _max(std::numeric_limits<double>::max()),
                     _prior((_name+"prior").c_str(),"1",_min,_max)
@@ -78,18 +82,42 @@ class PyFit
         };
     
     public:
+        struct Prediction
+        {
+            std::vector<double> entries;
+            Prediction(unsigned int N):
+                entries(N)
+            {
+            }
+            Prediction& operator+=(const Prediction& p)
+            {
+                if (entries.size()!=p.entries.size())
+                {
+                    throw std::runtime_error("attempting to add predictions with different sizes");
+                }
+                for (unsigned int i = 0; i < entries.size(); ++i)
+                {
+                    entries[i]+=p.entries[i];
+                }
+                return *this;
+            }
+        };
+    
+    
 
         class Component
         {
             public:
                 virtual void queryParameters(std::map<std::string, PyFit::Parameter*>& parameters) const = 0;
-                virtual void getPrediction(TH1D* hist) const = 0;
+                virtual void getPrediction(Prediction* p) const = 0;
                 
                 virtual ~Component()
                 {
                 }
                 
         };
+        
+
         
         class ConstShapeComponent:
             public Component
@@ -117,15 +145,22 @@ class PyFit
                     }
                 }
                 
-                virtual void getPrediction(TH1D* hist) const
+                virtual void getPrediction(Prediction* p) const
                 {
-                    
+                    if ((int)p->entries.size()!=_hist->GetNbinsX())
+                    {
+                        throw std::runtime_error("histogram in component has different number of bins");
+                    }
                     double combinedSF = 1.0;
                     for (const PyFit::Parameter* parameter: _parameters)
                     {
                         combinedSF*=parameter->getScaleFactor();
                     }
-                    hist->Add(_hist,combinedSF);
+                    for (unsigned int i = 0; i < p->entries.size(); ++i)
+                    {
+                        p->entries[i]+=combinedSF*_hist->GetBinContent(i+1);
+                    }
+                    
                     
                 }
                 
@@ -135,6 +170,8 @@ class PyFit
                 }
             ClassDef(PyFit::ConstShapeComponent, 1)
         };
+        
+
     
         class Observable
         {
@@ -145,8 +182,17 @@ class PyFit
                 {
                 }
                 
-                void getPrediction(TH1D* hist)
+                void getPrediction(Prediction* p)
                 {
+                    for (PyFit::Component* component: _components)
+                    {
+                        component->getPrediction(p);
+                    }
+                }
+                
+                void addComponent(PyFit::Component* component)
+                {
+                    _components.push_back(component);
                 }
                 
                 void queryParameters(std::map<std::string, PyFit::Parameter*>& parameters) const
@@ -157,10 +203,32 @@ class PyFit
                     }
                 }
                 
-                double logLikelihood(TH1D* data)
-                {
+                double nll(TH1D* data)
+                {   
+                    Prediction prediction(data->GetNbinsX());
+                    this->getPrediction(&prediction);
                     
-                    return 0;
+                    double ll = 0;
+                    for ( int ibin = 0; ibin < data->GetNbinsX(); ++ibin)
+                    {
+                        const double d = data->GetBinContent(ibin+1);
+                        const double p = prediction.entries[ibin];
+                        
+                        //using log of poisson distribution: x*log(lambda)-lambda
+                        //skip bins with no prediction
+                        if (p > 0.0)
+                        {
+                             if (d > 0.0)
+                             {
+                                 ll -= d * std::log(p)-p;
+                             }
+                         }
+                         else if (d > 0.0)
+                         {
+                             return std::numeric_limits<double>::max();
+                         }
+                    }
+                    return ll;
                 }
                 
                 virtual ~Observable()
@@ -172,12 +240,66 @@ class PyFit
         
     
     private:
-        std::vector<PyFit::Observable*> _observables;
+        std::vector<std::pair<PyFit::Observable*,TH1D*>> _observableDataPairs; //!
         
     
     public:
         PyFit()
         {
+        }
+        
+        void addObservable(PyFit::Observable* observable, TH1D* data)
+        {
+            _observableDataPairs.push_back(std::make_pair(observable,data));
+        }
+        
+        double globalNll() const
+        {
+            double nll = 0.0;
+            
+            for (auto pair: _observableDataPairs)
+            {
+                nll+=pair.first->nll(pair.second);
+            }
+            
+            return nll;
+        }
+        
+        void minimize()
+        {
+            std::map<std::string, PyFit::Parameter*> parameters;
+            for (auto pair: _observableDataPairs)
+            {
+                pair.first->queryParameters(parameters);
+            }
+            // Choose method upon creation between:
+            // kMigrad, kSimplex, kCombined, 
+            // kScan, kFumili
+            ROOT::Minuit2::Minuit2Minimizer min ( ROOT::Minuit2::kMigrad );
+
+            min.SetMaxFunctionCalls(1000000);
+            min.SetMaxIterations(100000);
+            min.SetTolerance(0.001);
+
+            ROOT::Math::Functor f(
+                [](const double *x) -> double 
+                {
+                    return this->globalNll();
+                },
+                parameters.size()
+            ); 
+
+            min.SetFunction(f);
+
+            // Set the free variables to be minimized!
+            min.SetVariable(0,"x",variable[0], step[0]);
+            //min.SetVariable(1,"y",variable[1], step[1]);
+
+            min.Minimize(); 
+
+            const double *xs = min.X();
+            std::cout<<xs[0]<<std::endl;
+
         }
         
         virtual ~PyFit()
--- scripts/PyTool/testPyFit.py
+++ scripts/PyTool/testPyFit.py
@@ -3,15 +3,31 @@
 import ROOT
 import math
 import random
+import numpy
 
 ROOT.gSystem.Load("libpyTool.so")
 
 fit = ROOT.PyFit()
 obs = ROOT.PyFit.Observable()
 par = ROOT.PyFit.Parameter("test")
-comp = ROOT.PyFit.ConstShapeComponent(ROOT.TH1D())
-comp.addSFParameter(par)
 
-print obs,par,comp
+data = ROOT.TH1D("data","",10,-1,1)
+for i in range(1000):
+    data.Fill(random.gauss(1,0.2))
+pred = data.Clone("prediction")
+
+for i in range(2):
+    comp = ROOT.PyFit.ConstShapeComponent(pred)
+    comp.addSFParameter(par)
+    obs.addComponent(comp)
+
+
+for sf in numpy.linspace(0.1,2,20):
+    par.setScaleFactor(sf)
+    print sf,obs.nll(data)
+    
+fit.minimize()
+
+#print obs,par,comp
 
 #obs.getPrediction(parameters).getLikelihood(dataHist)
