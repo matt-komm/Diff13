4310e43fe70208a137a45d85add2efd6ff4986f8
matt-komm <Matthias.Komm@cern.ch>
2016-01-31 22:09:38 +0100

    fixed met variations

--- PxlModules/systematics/JetMETVariations.cpp
+++ PxlModules/systematics/JetMETVariations.cpp
@@ -16,35 +16,38 @@ class JetMETVariations:
     public pxl::Module
 {
     private:
+        struct Variation
+        {
+            std::array<pxl::Source*,2> sources;
+            std::array<bool,2> active;
+            std::string inputJetName;
+            std::string inputMETName;
+        };
+    
         std::string _eventViewName;
     
         std::string _nominalJetName;
-        std::string _variationJetNamePrefix;
-        
         std::string _nominalMETName;
-        std::string _variationMETNamePrefix;
         
         std::string _renamedJet;
         std::string _renamedMET;
         
         bool _removeOtherVariations;
         
-        const static std::vector<std::string> variations;
+        const std::vector<std::string> _variationNames;
         pxl::Source* _nominalSource;
-        std::unordered_map<std::string, std::array<pxl::Source*,2>> _sources;
-        std::unordered_map<std::string, std::array<bool,2>> _activeVariations;
+        std::unordered_map<std::string, Variation> _variations;
         
     public:
         JetMETVariations():
             Module(),
             _eventViewName("Reconstructed"),
             _nominalJetName("JetRes"),
-            _variationJetNamePrefix("Jet"),
             _nominalMETName("METRes"),
-            _variationMETNamePrefix("METJet"),
             _renamedJet("Jet"),
             _renamedMET("MET"),
-            _removeOtherVariations(true)
+            _removeOtherVariations(true),
+            _variationNames({"Res","En","UnclEn"})
         {
             addSink("input", "input");
             
@@ -53,21 +56,24 @@ class JetMETVariations:
             addOption("nominal jet name","",_nominalJetName);
             addOption("nominal MET name","",_nominalMETName);
             
-            addOption("variation jet prefix","",_variationJetNamePrefix);
-            addOption("variation MET prefix","",_variationMETNamePrefix);
-            
             addOption("rename jet","",_renamedJet);
             addOption("rename MET","",_renamedMET);
             
             addOption("remove other variations","",_removeOtherVariations);
 
-            for (const std::string& variation: variations)
+            for (const std::string& name: _variationNames)
             {
-                _sources[variation]={addSource(variation+"Up",variation+"Up"),addSource(variation+"Down",variation+"Down")};
-                _activeVariations[variation]={true,true};
+                Variation variation;
+                variation.sources={addSource(name+"Up",name+"Up"),addSource(name+"Down",name+"Down")};
                 
-                addOption("variate "+variation+"Up","",_activeVariations[variation][0]);
-                addOption("variate "+variation+"Down","",_activeVariations[variation][1]);
+                addOption(name+" jet name","","");
+                addOption(name+" met name","","");
+
+                variation.active={true,true};
+                
+                addOption("variate "+name+"Up","",true);
+                addOption("variate "+name+"Down","",true);
+                _variations[name]=std::move(variation);
             }
             
             _nominalSource = addSource("nominal","nominal");
@@ -109,19 +115,19 @@ class JetMETVariations:
         
             getOption("nominal jet name",_nominalJetName);
             getOption("nominal MET name",_nominalMETName);
-
-            getOption("variation jet prefix",_variationJetNamePrefix);
-            getOption("variation MET prefix",_variationMETNamePrefix);
             
             getOption("rename jet",_renamedJet);
             getOption("rename MET",_renamedMET);
 
             getOption("remove other variations",_removeOtherVariations);
 
-            for (const std::string& variation: variations)
+            for (auto variation = _variations.begin(); variation!= _variations.end();++variation)
             {
-                getOption("variate "+variation+"Up",_activeVariations[variation][0]);
-                getOption("variate "+variation+"Down",_activeVariations[variation][1]);
+                getOption(variation->first+" jet name",variation->second.inputJetName);
+                getOption(variation->first+" met name",variation->second.inputMETName);
+                
+                getOption("variate "+variation->first+"Up",variation->second.active[0]);
+                getOption("variate "+variation->first+"Down",variation->second.active[1]);
             }
         }
         
@@ -132,6 +138,8 @@ class JetMETVariations:
             event->getObjectsOfType(eventViews);
             
             bool metRenamed = false;
+            
+            pxl::Particle* nominalMET = nullptr;
             
             for (pxl::EventView* eventView: eventViews)
             {
@@ -154,21 +162,36 @@ class JetMETVariations:
                             }
                             metRenamed=true;
                         }
+                        else if (particle->getName()==_nominalMETName)
+                        {
+                            nominalMET = particle;
+                        }
+                        
+                        //remove particle if it begins with _renamedJet
                         else if (_removeOtherVariations and std::equal(_renamedJet.begin(),_renamedJet.end(),particle->getName().begin()))
                         {
                             eventView->removeObject(particle);
                         }
+                        //remove particle if it begins with _renamedMET
                         else if (_removeOtherVariations and std::equal(_renamedMET.begin(),_renamedMET.end(),particle->getName().begin()))
                         {
                             eventView->removeObject(particle);
                         }
+                        
+                    }
+                    
+                    //need MET in event for neutrino pz
+                    if (not metRenamed)
+                    {
+                        nominalMET->setName(_renamedMET);
+                    }
+                    else if (nominalMET)
+                    {
+                        eventView->removeObject(nominalMET);
                     }
                 }
             }
-            if (!metRenamed)
-            {
-                throw std::runtime_error("No MET object found of name '"+metName+"'");
-            }
+
         }
 
         bool analyse(pxl::Sink *sink) throw (std::runtime_error)
@@ -180,30 +203,50 @@ class JetMETVariations:
                 if (event)
                 {
                     bool success = true;
-                    for (const std::string& variation: variations)
+                    for (auto variation = _variations.begin(); variation!= _variations.end();++variation)
                     {
-                        if (_activeVariations[variation][0])
+                        if (variation->second.active[0])
                         {
                             pxl::Event* eventShiftedUp = dynamic_cast<pxl::Event*>(event->clone());
                             if (not eventShiftedUp)
                             {
                                 throw std::runtime_error("cloning of Event failed");
                             }
-                            renameJetMET(eventShiftedUp,_variationJetNamePrefix+variation+"Up",_variationMETNamePrefix+variation+"Up");
-                            _sources[variation][0]->setTargets(eventShiftedUp);
-                            success &= _sources[variation][0]->processTargets();
+                            std::string jetName = variation->second.inputJetName+"Up";
+                            if (variation->second.inputJetName=="")
+                            {
+                                jetName=_nominalJetName;
+                            }
+                            std::string metName = variation->second.inputMETName+"Up";
+                            if (variation->second.inputMETName=="")
+                            {
+                                metName=_nominalMETName;
+                            }
+                            renameJetMET(eventShiftedUp,jetName,metName);
+                            variation->second.sources[0]->setTargets(eventShiftedUp);
+                            success &= variation->second.sources[0]->processTargets();
                             delete eventShiftedUp;
                         }
-                        if (_activeVariations[variation][1])
+                        if (variation->second.active[1])
                         {
                             pxl::Event* eventShiftedDown = dynamic_cast<pxl::Event*>(event->clone());
                             if (not eventShiftedDown)
                             {
                                 throw std::runtime_error("cloning of Event failed");
                             }
-                            renameJetMET(eventShiftedDown,_variationJetNamePrefix+variation+"Down",_variationMETNamePrefix+variation+"Down");
-                            _sources[variation][1]->setTargets(eventShiftedDown);
-                            success &= _sources[variation][1]->processTargets();
+                            std::string jetName = variation->second.inputJetName+"Down";
+                            if (variation->second.inputJetName=="")
+                            {
+                                jetName=_nominalJetName;
+                            }
+                            std::string metName = variation->second.inputMETName+"Down";
+                            if (variation->second.inputMETName=="")
+                            {
+                                metName=_nominalMETName;
+                            }
+                            renameJetMET(eventShiftedDown,jetName,metName);
+                            variation->second.sources[1]->setTargets(eventShiftedDown);
+                            success &= variation->second.sources[1]->processTargets();
                             delete eventShiftedDown;
                         }
                     }
@@ -242,11 +285,6 @@ class JetMETVariations:
         }
 };
 
-const std::vector<std::string> JetMETVariations::variations = {
-"Res",
-"En",
-"UnclEn"
-};
 
 PXL_MODULE_INIT(JetMETVariations)
 PXL_PLUGIN_INIT
