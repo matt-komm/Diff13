f8165accb9026ad21db4edac87776067bc8287ff
matt-komm <mkomm@cern.ch>
2016-02-01 14:38:37 +0100

    updated EW

--- PxlModules/utils/EventWeight.cpp
+++ PxlModules/utils/EventWeight.cpp
@@ -16,9 +16,8 @@ struct FileInfo
     double crossSection;
 };
 
-const std::unordered_map<std::string,FileInfo> eventWeights = {
+const std::unordered_map<std::string,FileInfo> eventWeightsPHYS14 = {
     //PHYS14
-    /*
     {"TToLeptons_t-channel-CSA14_Tune4C_13TeV-aMCatNLO-tauola",
         {
             1037660,
@@ -151,11 +150,12 @@ const std::unordered_map<std::string,FileInfo> eventWeights = {
             4581841,
             18.81
         }
-    },
-    */
+    }
+};
+
+const std::unordered_map<std::string,FileInfo> eventWeightsEA = {
     
     //DR74 50ns
-    /*
     {"ST_t-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1",
         {
             860070,
@@ -216,8 +216,11 @@ const std::unordered_map<std::string,FileInfo> eventWeights = {
             13227148,
             866600000 * 0.00044
         }
-    },
-    */
+    }
+};
+
+
+const std::unordered_map<std::string,FileInfo> eventWeights74X = {
     
     //DR74 25ns
     {"ST_t-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1",
@@ -331,8 +334,194 @@ const std::unordered_map<std::string,FileInfo> eventWeights = {
             13247363,
             866600000 * 0.00044
         }
+    }
+};
+
+const std::unordered_map<std::string,FileInfo> eventWeights76X = {
+    //DR76 25ns
+    {"ST_t-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1",
+        {
+            //total=19938230 eff=12117697-7820533=4297164
+            4297164,
+            216.97 * 0.324
+        }
+    },
+    {"ST_t-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1_ext",
+        {
+            //total=29257464 eff=17779834-11477630=6302204
+            6302204,
+            216.97 * 0.324
+        }
+    },
+    /*
+    {"ST_t-channel_top_4f_leptonDecays_13TeV-powheg-pythia8_TuneCUETP8M1",
+        {
+            //total=3299800 eff=3299800-0=3299800
+            
+            136.02 * 0.324
+        }
+    },
+    {"ST_t-channel_antitop_4f_leptonDecays_13TeV-powheg-pythia8_TuneCUETP8M1",
+        {
+            //total=1680200 eff=1680200-0=1680200
+            
+            80.95 * 0.324
+        }
+    },
+    */
+    
+    {"ST_t-channel_5f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1",
+        {
+            //total=2995200 eff=2063570-931630=1131940
+            1131940,
+            216.97 * 0.324
+        }
     },
     
+    {"ST_t-channel_4f_mtop1695_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1",
+        {
+            //total=2971601 eff=1806050-1165551=640499
+            640499,
+            216.97 * 0.324
+        }
+    
+    },
+    
+    {"ST_t-channel_4f_mtop1755_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1",
+        {
+            //total=2999200 eff=1823285-1175915=647370
+            647370,
+            216.97 * 0.324
+        }
+    },
+    
+    
+    {"ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1",
+        {
+            //total=1000000 eff=1000000-0=1000000
+            1000000,
+            35.6
+        }
+    },
+    {"ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1",
+        {
+            //total=482200 eff=482200-0=482200
+            482200,
+            35.6
+        }
+    },
+    
+    {"ST_tW_top_5f_mtop1695_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1",
+        {
+            //total=492500 eff=492500-0=492500
+            492500,
+            35.6
+        }
+    },
+    {"ST_tW_top_5f_mtop1755_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1",
+        {
+            //total=484800 eff=484800-0=484800
+            484800,
+            35.6
+        }
+    },
+    
+    {"ST_tW_antitop_5f_mtop1695_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1",
+        {
+            //total=499200 eff=499200-0=499200
+            499200,
+            35.6
+        }
+    },
+    {"ST_tW_antitop_5f_mtop1755_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1",
+        {
+            //total=482200 eff=482200-0=482200
+            482200,
+            35.6
+        }
+    },
+    
+    
+    /*
+    {"TT_TuneCUETP8M1_13TeV-powheg-pythia8",
+        {
+            //total=19757190 eff=19757190-0=19757190
+            
+            831.76
+        }
+    },
+    */
+    {"TT_TuneCUETP8M1_13TeV-powheg-pythia8_ext",
+        {
+            //total=97994442 eff=97994442-0=97994442
+            97994442,
+            831.76
+        }
+    },
+    {"TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8",
+        {
+            //total=19090400 eff=14232207-4858193=9374014
+            9374014,
+            831.76
+        }
+    },
+    /*
+    {"TT_TuneZ2star_13TeV-powheg-pythia6-tauola",
+        {
+            //total=18666925 eff=18666925-0=18666925
+            
+            831.76
+        }
+    },
+    */
+    
+    {"WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8",
+        {
+            //total=24156124 eff=20338633-3817491=16521142
+            16521142,
+            20508.9*3
+        }
+    },
+    /*
+    {"WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+        {
+            //total=72207128 eff=72207128-0=72207128
+            
+            20508.9*3
+        }
+    },
+    */
+    {"DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_ext",
+        {
+            //total=1719818 eff=1433002-286816=1146186
+            1146186,
+            2008.4*3
+        }
+    },
+    
+    {"QCD_Pt-20toInf_MuEnrichedPt15_TuneCUETP8M1_13TeV_pythia8",
+        {
+            //total=21846364 eff=21846364-0=21846364
+            21846364,
+            866600000 * 0.00044
+        }
+    }
+};
+
+struct Era
+{
+    enum TYPE
+    {
+        MCPHYS14=0, MCEA=1, MC74X=2, MC76X=3
+    };
+};
+        
+const std::unordered_map<int,const std::unordered_map<std::string,FileInfo>> _eventWeightsPerEra =
+{
+    {Era::MCPHYS14,eventWeightsPHYS14},
+    {Era::MCEA,eventWeightsEA},
+    {Era::MC74X,eventWeights74X},
+    {Era::MC76X,eventWeights76X}
 };
 
 class EventWeight:
@@ -345,6 +534,8 @@ class EventWeight:
         std::vector<std::string> _allowedPostfixes;
         
         std::vector<std::string> _processNameList;
+        
+        Era::TYPE _eraWeightType;
 
     public:
     
@@ -357,7 +548,11 @@ class EventWeight:
             _outputSource = addSource("output","output");
             
             addOption("name of process field","",_processNameField);
-            addOption("allowed postfixes","",_allowedPostfixes);
+            
+            addOption("use PHYS14","",false);
+            addOption("use EA","",false);
+            addOption("use 74X","",false);
+            addOption("use 76X","",true);
         }
 
         ~EventWeight()
@@ -392,7 +587,44 @@ class EventWeight:
             getOption("name of process field",_processNameField);
             getOption("allowed postfixes",_allowedPostfixes);
             
-            for (auto it = eventWeights.begin(); it!=eventWeights.end(); ++it)
+            bool usePHYS14 = false;
+            bool useEA = false;
+            bool use74X = false;
+            bool use76X = false;
+            
+            
+            getOption("use PHYS14",usePHYS14);
+            getOption("use EA",useEA);
+            getOption("use 74X",use74X);
+            getOption("use 76X",use76X);
+            std::string eraName = "";
+            if (usePHYS14)
+            {
+                _eraWeightType = Era::MCPHYS14;
+                eraName="PHYS14";
+            }
+            else if (useEA)
+            {
+                _eraWeightType = Era::MCEA;
+                eraName="EA";
+            }
+            else if (use74X)
+            {
+                _eraWeightType = Era::MC74X;
+                eraName="74X";
+            }
+            else if (use76X)
+            {
+                _eraWeightType = Era::MC76X;
+                eraName="76X";
+            }
+            else
+            {
+                throw std::runtime_error("Event weight era not set");
+            }
+            logger(pxl::LOG_LEVEL_INFO , "Event weights taken from '"+eraName+"'");
+            
+            for (auto it = _eventWeightsPerEra.at(_eraWeightType).cbegin(); it != _eventWeightsPerEra.at(_eraWeightType).cend(); ++it)
             {
                 _processNameList.push_back(it->first);
             }
@@ -405,6 +637,7 @@ class EventWeight:
                 pxl::Event *event  = dynamic_cast<pxl::Event*>(sink->get());
                 if (event)
                 {
+                    
                     std::string processName = event->getUserRecord(_processNameField);
                     bool found = false;
                     for (std::string& possibleName: _processNameList)
@@ -418,8 +651,8 @@ class EventWeight:
 
                     if (found)
                     {
-	                    auto it = eventWeights.find(processName);
-                        if (it!=eventWeights.end())
+	                    auto it = _eventWeightsPerEra.at(_eraWeightType).find(processName);
+                        if (it!=_eventWeightsPerEra.at(_eraWeightType).end())
                         {
 	                        event->setUserRecord("mc_weight",1.0*it->second.crossSection/it->second.nEvents);
                             found = true;
@@ -431,6 +664,7 @@ class EventWeight:
                     }
                     _outputSource->setTargets(event);
                     return _outputSource->processTargets();
+                    
                 }
             }
             catch(std::exception &e)
