b6086c3fe8e98104322191533b0a00e7f1ac1f4f
matt-komm <mkomm@cern.ch>
2016-03-07 19:07:38 +0100

    fixed find depth

--- setVars.sh
+++ setVars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PWD=`pwd`
-for file in `find $PWD -name "*-vars.txt"`
+for file in `find $PWD -maxdepth 1 -name "*-vars.txt"`
     do
     echo "reading "$file" ..."
     for l in `cat $file`
